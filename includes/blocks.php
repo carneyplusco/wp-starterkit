a342b6824bcdfd0b5b9d65ef11b9fd14c6790d22
Amy <amy.depalma@carney.co>
2022-02-10 11:49:51 -0500

    Updated the removal of the block patterns location

--- includes/blocks.php
+++ includes/blocks.php
@@ -1,9 +1,5 @@
 <?php
 
-// Remove default Block Editor patterns, custom font sizing
-remove_theme_support("core-block-patterns");
-add_theme_support("disable-custom-font-sizes");
-
 // Custom ACF Blocks
 add_action('acf/init', function () {
   if (function_exists('acf_register_block_type')):
