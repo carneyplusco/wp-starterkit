18c180636296811627e8ca4dd9232d3688419971
Amy <amy.depalma@carney.co>
2022-07-22 08:52:40 -0400

    Added the heading and paragraph styles by default to the kit

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -5,4 +5,24 @@ wp.domReady(() => {
   // Remove default WP Column blocks to avoid confusion with our own blocks
   wp.blocks.unregisterBlockType("core/columns");
   wp.blocks.unregisterBlockType("core/column");
+
+  // Typography
+  [1, 2, 3, 4, 5, 6].forEach((n) => {
+    wp.blocks.registerBlockStyle("core/heading", {
+      name: `h${n}`,
+      label: `H${n} Size`,
+    });
+
+    wp.blocks.registerBlockStyle("core/paragraph", {
+      name: `h${n}`,
+      label: `H${n} Size`,
+    });
+  });
+
+  wp.blocks.registerBlockStyle("core/paragraph", [
+    {
+      name: "lead",
+      label: "Lead",
+    },
+  ]);
 });
