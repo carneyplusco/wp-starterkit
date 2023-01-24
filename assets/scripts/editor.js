f908b1f9660a07885700e2459a84ccdefa4df827
Kirby Johnston <kirby.johnston@carney.co>
2023-01-24 13:21:23 -0500

    working on block registration

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -1,35 +1,49 @@
+import { registerBlockType } from "@wordpress/blocks";
 /**
  * Custom block styles
  */
 wp.domReady(() => {
-  // Remove default WP Column blocks to avoid confusion with our own blocks
-  wp.blocks.unregisterBlockType("core/columns");
-  wp.blocks.unregisterBlockType("core/column");
+	// Remove default WP Column blocks to avoid confusion with our own blocks
+	wp.blocks.unregisterBlockType("core/columns");
+	wp.blocks.unregisterBlockType("core/column");
 
-  // Typography
-  [1, 2, 3, 4, 5, 6].forEach((n) => {
-    wp.blocks.registerBlockStyle("core/heading", {
-      name: `h${n}`,
-      label: `H${n} Size`,
-    });
+	// Typography
+	[1, 2, 3, 4, 5, 6].forEach((n) => {
+		wp.blocks.registerBlockStyle("core/heading", {
+			name: `h${n}`,
+			label: `H${n} Size`,
+		});
 
-    wp.blocks.registerBlockStyle("core/paragraph", {
-      name: `h${n}`,
-      label: `H${n} Size`,
-    });
-  });
+		wp.blocks.registerBlockStyle("core/paragraph", {
+			name: `h${n}`,
+			label: `H${n} Size`,
+		});
+	});
 
-  wp.blocks.registerBlockStyle("core/paragraph", [
-    {
-      name: "lead",
-      label: "Lead",
-    },
-  ]);
+	wp.blocks.registerBlockStyle("core/paragraph", [
+		{
+			name: "lead",
+			label: "Lead",
+		},
+	]);
 
-  wp.blocks.registerBlockStyle("core/paragraph", [
-    {
-      name: "small",
-      label: "Small",
-    },
-  ]);
+	wp.blocks.registerBlockStyle("core/paragraph", [
+		{
+			name: "small",
+			label: "Small",
+		},
+	]);
 });
+
+(function (blocks, element) {
+	var el = element.createElement;
+
+	blocks.registerBlockType("gutenberg-examples/example-01-basic", {
+		edit: function () {
+			return el("p", {}, "Hello World (from the editor).");
+		},
+		save: function () {
+			return el("p", {}, "Hola mundo (from the frontend).");
+		},
+	});
+})(window.wp.blocks, window.wp.element);
--- includes/blocks.php
+++ includes/blocks.php
@@ -1,7 +1,12 @@
 <?php
-
 // Custom ACF Blocks
 add_action('acf/init', function () {
+	die(var_dump('should fucking die'));
   if (function_exists('acf_register_block_type')):
+	die(var_dump('fuck'));
+
+
+	
+	
   endif;
 });
