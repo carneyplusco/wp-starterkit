275de38155bcfa83089f8df12623170c45c78fc0
Kirby Johnston <kirby.johnston@carney.co>
2023-01-24 11:59:13 -0500

    updates to hygen script

--- _templates/scaffold/new/prompt.js
+++ _templates/scaffold/new/prompt.js
@@ -2,19 +2,32 @@
 // https://github.com/enquirer/enquirer#built-in-prompts
 
 module.exports = [
-  {
-    type: 'input',
-    name: 'theme_name',
-    message: "What's your theme name?"
-  },
-  {
-    type: 'input',
-    name: 'theme_description',
-    message: "Add a description for your theme"
-  },
-  {
-    type: 'list',
-    name: 'theme_colors',
-    message: 'Add theme colors, comma separated (e.g. blue: #978edf, red: #d73b23)'
-  }
-]
+	{
+		type: "input",
+		name: "theme_name",
+		message: "What's your theme name?",
+	},
+	{
+		type: "input",
+		name: "theme_description",
+		message: "Add a description for your theme",
+	},
+	{
+		type: "list",
+		name: "theme_colors",
+		message:
+			"Add theme colors, comma separated (e.g. blue: #978edf, red: #d73b23)",
+	},
+	{
+		type: "input",
+		name: "staging_server",
+		message:
+			"What is the name of the staging server where this will be deployed?",
+	},
+	{
+		type: "input",
+		name: "application_name",
+		message:
+			"What is the name of the project on the server? This may be subtly different than the theme name.",
+	},
+];
