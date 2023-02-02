e523c4a1405bad354f98ba18bdcd164d68f0fbe7
Kirby Johnston <kirby.johnston@carney.co>
2023-02-02 12:43:36 -0500

    adds more opinionated javascript boilerplate

--- blocks/example-block/example-block.js
+++ blocks/example-block/example-block.js
@@ -1,1 +1,17 @@
-console.log("example-block");
+
+window.addEventListener('DOMContentLoaded', () => {
+	const isAdmin = window.acf !== undefined;
+
+	if (document.readyState == 'interactive' || document.readystate == 'complete') {
+			initExampleBlock(isAdmin);
+	}
+})
+
+function initExampleBlock(isAdmin) {
+	// add block code here
+	if (isAdmin) {
+		console.log("new-block block ready in editor");
+	} else {
+		console.log("new-block block ready in front end");
+	}
+}
