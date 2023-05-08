15d8075884270d7ae508ca66be70657b5c632812
Kirby Johnston <kirby.johnston@carney.co>
2023-05-08 16:43:38 -0400

    clarified, adds guard clauses for block json js and css, adds registration of acf fields within block directories

--- includes/blocks.php
+++ includes/blocks.php
@@ -1,15 +1,5 @@
 <?php
 
-// function carney_register_block_type($settings)
-// {
-//   $needs_callback = empty($settings['render_template']) && empty($settings['render_callback']);
-
-//   if ($needs_callback) :
-//     $settings['render_callback'] = "carney_render_block";
-//   endif;
-//   acf_register_block_type($settings);
-// }
-
 function carney_block($block_name, $args = [])
 {
   	$template_path = "blocks/$block_name/$block_name.php";
@@ -46,13 +36,35 @@ function carney_render_block($block, $content = '', $is_preview = false, $post_i
   	carney_block($block_name, $args);
 };
 
-// Custom ACF Blocks
+// auto register custom blocks within blocks directory
 add_action('acf/init', function () {
-	// auto register all custom blocks 
-	foreach ( glob( get_stylesheet_directory() . "/blocks/*/block.json" ) as $file ) {
-		$block_name = basename(dirname($file));
-		register_block_type( get_template_directory() . '/blocks/' . $block_name . '/block.json', array( 'render_callback' => 'carney_render_block') );
-		wp_register_script( $block_name, get_template_directory_uri() . '/dist/blocks/' . $block_name . '/' . $block_name . '.js', [ 'acf' ] );
-		wp_register_style( $block_name, get_template_directory_uri() . '/dist/blocks/' . $block_name . '/' . $block_name . '.css');
+	foreach (glob(get_stylesheet_directory() . '/blocks/*/') as $block_dir) {
+		$block_name = basename($block_dir);
+		$block_json = $block_dir . 'block.json';
+		$block_dist_path = trailingslashit(get_stylesheet_directory() . '/dist/blocks/' . $block_name);
+		$block_dist_uri = trailingslashit(get_template_directory_uri() . '/dist/blocks/' . $block_name);
+
+		if (file_exists($block_json)) {
+			register_block_type($block_json, array('render_callback' => 'carney_render_block'));
+		}
+
+		if (file_exists($block_dist_path . $block_name . '.js')) {
+			wp_register_script($block_name, $block_dist_uri . $block_name . '.js', array('acf'), filemtime($block_dist_path . $block_name . '.js'), false);
+		}
+
+		if (file_exists($block_dist_path . $block_name . '.css')) {
+			wp_register_style($block_name, $block_dist_uri . $block_name . '.css', array(), filemtime($block_dist_path . $block_name . '.css'), false);
+		}
 	}
 });
+
+// auto register custom block acf fields if they exist within block directories
+foreach (glob(get_stylesheet_directory() . '/blocks/*/') as $block_dir) {
+	add_filter(
+		'acf/settings/load_json',
+		function ($paths) use ($block_dir) {
+			$paths[] = $block_dir;
+			return $paths;
+		}
+	);
+}
